332ab7307c1e5dca2a5d29001cf73af5ff2d882e
Arthur Nagy <arthur.nagy@m-kopa.com>
2024-12-30 00:20:58 +0200

    Fix env vars not being recognized

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,7 +3,7 @@ import { Resend } from 'resend'
 import { headers } from 'next/headers'
 import { rateLimit } from '@/utils/rateLimiter'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY || process.env.NEXT_PUBLIC_RESEND_API_KEY)
 
 export async function POST(request: Request) {
   try {
